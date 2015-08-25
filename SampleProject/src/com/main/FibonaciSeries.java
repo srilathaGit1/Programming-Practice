c8bf696847fa0bd0ec8434c8b11eaec5ba6409fd
srilatha <srilatha.ip1@gmail.com>
2015-08-24 22:08:09 -0700

    Finished FibonaciSeries and TestFibonaciSeries.

---
+++ SampleProject/src/com/main/FibonaciSeries.java
@@ -0,0 +1,39 @@
+package com.main;
+
+//Write a program that prints a fibonaci series that is a sequence of numbers like0 1 1 2 3 5 8.You can vary the number of elements to be printed meaning you can print 10 numbers or 15 or 20 or any desired number.
+
+
+public class FibonaciSeries {
+	int n;
+	int prev1;
+	int prev2;
+
+	public  int isFibonaciSeries(int N){
+		
+		this.n = N;
+		prev1 = 0;
+		prev2 = 1;
+		for(int i =0; i<N; ++i){
+			int savprev1=prev1;
+			prev1 = prev2;
+			prev2 = savprev1+prev1;
+
+		}
+		return prev1;
+
+	}
+	
+	public static int isFibonaciSeries1(int N){
+         int prev1 = 0;
+         int prev2 = 1;
+		for(int i =0; i<N; i++){
+			int savprev1=prev1;
+			prev1 = prev2;
+			prev2 = savprev1+prev1;
+		}
+		return prev1;
+	}
+
+	// TODO Auto-generate
+
+}
---
+++ SampleProject/src/com/test/TestFibonaciSeries.java
@@ -0,0 +1,32 @@
+package com.test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.main.FibonaciSeries;
+
+public class TestFibonaciSeries {
+	
+  @Test
+  public void isFibonaci() {
+	  
+	FibonaciSeries feb = new FibonaciSeries();
+	int result = feb.isFibonaciSeries(10);
+	
+	/* for (int i=2; i<=10; i++){
+		 int result = feb.isFibonaciSeries(i);
+		 System.out.println(result); 
+	 }*/
+
+  }
+  @Test
+  public void isFibonaciforTwenty(){
+	  
+	  for (int i=2; i<=20; i++){
+		  int result = FibonaciSeries.isFibonaciSeries1(i);
+		System.out.println(result);
+	  }
+	  
+  }   
+	  
+  }
+  
+
