875ec3f4c078c19927ecfa7cf70ce74f0d21558d
srilatha <srilatha.ip1@gmail.com>
2015-09-23 09:28:59 -0700

    Implemented interface for Shape Constants

---
+++ SampleProject/src/com/main/ShapeConstants.java
@@ -0,0 +1,11 @@
+// Write a program creating an inteface ShapeConstants with variable(pi=3.14).
+
+package com.main;
+
+public interface ShapeConstants {
+	
+	public static final  double pi=3.14;
+	
+  
+
+}
