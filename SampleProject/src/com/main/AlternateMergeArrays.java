44a079b15cdc2d41835828a474ee472cd6b9ab0a
srilatha <srilatha.ip1@gmail.com>
2015-09-23 09:08:44 -0700

    Implemented abstract class for Shape With Properties.

--- SampleProject/src/com/main/AlternateMergeArrays.java
+++ SampleProject/src/com/main/AlternateMergeArrays.java
@@ -5,7 +5,7 @@ public class AlternateMergeArrays {
 	public String[] mergeArrays(String[] parr, String[] parr1){
 		String[] merge = new String[parr.length+parr1.length];
 		int mergeCounter = -1;
-		for(int i=0; i<merge.length; i++){
+		for(int i=0; i < merge.length; i++){
 			if(i<parr.length){
 				mergeCounter++;
 				merge[mergeCounter] = parr[i];
---
+++ SampleProject/src/com/main/ShapeWithProperties.java
@@ -0,0 +1,25 @@
+
+//Write a program creating an abstract class Shape with properties
+//(noOfSides,area,perimeter) and methods(calculateArea,calculatePerimeter,setSides.
+package com.main;
+
+public abstract class ShapeWithProperties {
+	
+	private int noOfSides;
+	private double area;
+	private double perimeter;
+	
+	public double calculateArea(){
+		return area;	
+	}
+	
+	public double calculatePerimeter(){
+		return 	perimeter;	
+	}
+	
+	public void setSides( int pSides){
+		this.noOfSides = pSides;
+	}
+	
+
+}
