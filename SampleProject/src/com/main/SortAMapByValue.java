82d00ddcf33ec115ad00c53b0e372a1ac28bf9f9
srilatha <srilatha.ip1@gmail.com>
2015-09-28 10:53:21 -0700

    Created a HashMap

---
+++ SampleProject/src/com/main/SortAMapByValue.java
@@ -0,0 +1,18 @@
+package com.main;
+
+import java.util.HashMap;
+
+public class SortAMapByValue {
+	
+	public void sortHashMap(HashMap myMap, int pKey, int pKey1){
+		
+		
+		
+		
+		
+			
+		}
+	
+	
+
+}
