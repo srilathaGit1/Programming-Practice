a083f30336547658374d467fac0e80b5bb5c8d8c
srilatha <srilatha.ip1@gmail.com>
2015-09-28 23:49:51 -0700

    class for access modifiers.

--- SampleProject/src/com/main/Person.java
+++ SampleProject/src/com/main/Person.java
@@ -32,4 +32,16 @@ public class Person {
  public String getDisplayName(){
 	 return firstName+ " " +lastName;
  }
+ // private modifier only accessible with in the class.
+ private int getEmployeeSSN(){
+	 return 123445;
+ }
+ //protected modifier can be accessible outside of the package only by inheritance.
+ protected String getCountryName(){
+	 return "India";
+ }
+ //default modifier cannot access outside of the package. Only accessible within the package.
+ String getCityName(){
+	 return "Hyderabad";
+ }
 }
